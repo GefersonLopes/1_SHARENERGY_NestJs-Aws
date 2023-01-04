b7c462ff45c6dbe4d3713fdc6d5e1c764552300f
Geferson <gefersonjefreey@gmail.com>
2023-01-04 04:06:26 -0300

    firt commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ClientsModule } from './clients/clients.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+const link_db = `${process.env.DB}+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@${process.env.CLUSTER_DB}.etca6ii.mongodb.net/?retryWrites=true&w=majority`;
+
+@Module({
+  imports: [
+    UsersModule,
+    ClientsModule,
+    MongooseModule.forRoot(link_db),
+    AuthModule,
+  ],
+})
+export class AppModule {}
